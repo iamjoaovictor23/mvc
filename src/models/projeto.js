fabf450f30e7de7c84be05dee59148296259f007
iamjoaovictor23 <viictorcabraal@outlook.com>
2023-09-14 20:40:48 -0300

    adicionando mongoose

---
+++ src/models/projeto.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';    
+
+const projetoSchema = new mongoose.Schema(
+    {
+        nome: { type: String, required: true },
+
+    }
+)
+
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+const projetos = mongoose.model('projetos', projetoSchema)
+
+export default projetos;
